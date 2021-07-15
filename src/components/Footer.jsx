53b7c11be1922ab3ade3ac3e994f397ce63699a5
shiv-chan <kshibuya31@gmail.com>
2021-07-15 16:33:05 -0700

    Style footer component.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,10 +4,18 @@ import { FiMail, FaGithub, FaLinkedin, FaDev } from 'react-icons/all';
 export default function Footer() {
 	return (
 		<footer>
-			<FiMail />
-			<FaGithub />
-			<FaLinkedin />
-			<FaDev />
+			<a href="mailto: kshibuya31@gmail.com">
+				<FiMail />
+			</a>
+			<a href="https://github.com/shiv-chan">
+				<FaGithub />
+			</a>
+			<a href="https://www.linkedin.com/in/kaho-shibuya">
+				<FaLinkedin />
+			</a>
+			<a href="https://dev.to/shivchan">
+				<FaDev />
+			</a>
 		</footer>
 	);
 }
